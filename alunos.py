bb43cbad924110f942401a4175dc9038237ba349
valeria <silvvavaleria@gmail.com>
2020-04-16 20:45:04 -0300

    Including tutorial Streamlit

--- alunos.py
+++ alunos.py
@@ -1,12 +1,53 @@
+#Streamlit -> Primeiros passos
 import streamlit as st
 
 def main():
-    st.title("Hello Valeria")
-    st.header("Header")
-    st.subheader("This is subheader")
-    st.text("This is text")
-    st.image("logo.png")
-    st.video("https://www.youtube.com/watch?v=TF3ieRS4kis")
-if __name__ == '__main__':
-	main()
 
+    #Titulo
+    st.title("Hello Valéria Imperatriz do 41")
+
+    #Criando um botão
+    st.markdown('Botao')
+    botao = st.button('Botao')
+    if botao:
+        st.markdown('Clicado')
+
+    #Criando um checkbox
+    check = st.checkbox('Checkbox')
+    st.markdown(check)
+    if check:
+        st.markdown('Clicado')
+
+    #Criando um radio
+    st.markdown('Radio')
+    radio = st.radio('Escolha as opções', ('Opt1', 'Opt2'))
+    if radio == 'Opt1':
+        st.markdown('Opt1')
+    if radio == 'Opt2':
+        st.markdown('Opt2')
+
+    #Criando um SelectBox
+    st.markdown('Selectbox')
+    select = st.selectbox('Choose opt', ('Opt1', 'Opt2'))
+    if select == 'Opt1':
+        st.markdown('Opt1')
+    if select == 'Opt2':
+        st.markdown('Opt2')
+
+    #Multiselect
+    st.markdown('Multiselect')
+    multi = st.multiselect('Choose opt', ('Opt1', 'Opt2'))
+    if multi == 'Opt1':
+        st.markdown('Opt1')
+    if multi == 'Opt2':
+        st.markdown('Opt2')
+
+    #Fazendo Upload de Arquivo
+    st.markdown('File Uploader')
+    file = st.file_uploader('Choose your file', type = 'csv')
+    if file is not None:
+        st.markdown("Não está vazio")
+
+
+if __name__ == '__main__':
+    main()
