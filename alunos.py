0120ba6339f80eee5bffc972d0ce776cb2af34b2
valeria <silvvavaleria@gmail.com>
2020-04-16 20:17:53 -0300

    Introducion in Streamlit

---
+++ alunos.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def main():
+    st.title("Hello Valeria")
+    st.header("Header")
+    st.subheader("This is subheader")
+    st.text("This is text")
+    st.image("logo.png")
+    st.video("https://www.youtube.com/watch?v=TF3ieRS4kis")
+if __name__ == '__main__':
+	main()
+
