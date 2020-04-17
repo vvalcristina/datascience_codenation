d93a1c675e7cfd2b708a128976902513bb554b9e
valeria <silvvavaleria@gmail.com>
2020-04-16 21:13:18 -0300

    Media no Streamlit

--- semana2s.py
+++ semana2s.py
@@ -9,13 +9,18 @@ def main():
     file = st.file_uploader('Upload your file', type = 'csv')
     if file is not None:
         slider = st.slider('Valores', 1,100)
+        #importando Df
         df = pd.read_csv(file)
         st.dataframe(df.head(slider))
 
         st.markdown('Markdown')
         st.table(df.head(slider))
 
+        #Retorna o nome das colunas
         st.write(df.columns)
+
+        #Retorna a média
+        st.table(df.groupby('species')['petal_width'].mean())
 
 
 if __name__ == '__main__':
