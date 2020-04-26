af60f914f6bf7f3d0dddab57d8d161d36fd79481
valeria <silvvavaleria@gmail.com>
2020-04-26 13:03:26 -0300

    Teste

--- Semana2/semana2s.py
+++ Semana2/semana2s.py
@@ -1,12 +1,13 @@
-#Aplicação desenvolvida pelo professor durante a Semana 2
+# Aplicação desenvolvida pelo professor durante a Semana 2
 
-#Importações
+# Importações
 import streamlit as st
 import pandas as pd
-#Para fazer Download (Desenvolvida pela comunidade do Streamlit)
+# Para fazer Download (Desenvolvida pela comunidade do Streamlit)
 import base64
 
-#Função de download
+
+# Função de download
 def get_table_download_link(df):
     """Generates a link allowing the data in a given panda dataframe to be downloaded
     in:  dataframe
@@ -17,18 +18,19 @@ def get_table_download_link(df):
     href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
     return href
 
+
 def main():
-    st.image('logo.png', width= 200)
+    st.image('logo.png', width=200)
     st.title('AceleraDev Data Science')
     st.subheader('Semana 2 - Pré-processamento de Dados em Python')
     st.image('https://media.giphy.com/media/KyBX9ektgXWve/giphy.gif', width=200)
-    file  = st.file_uploader('Escolha a base de dados que deseja analisar (.csv)', type = 'csv')
+    file = st.file_uploader('Escolha a base de dados que deseja analisar (.csv)', type='csv')
     if file is not None:
         st.subheader('Analisando os dados')
-        #Leitura dos arquivos
+        # Leitura dos arquivos
         df = pd.read_csv(file)
 
-        #Shape: me retorna uma tupla
+        # Shape: me retorna uma tupla
         st.markdown('**Número de linhas:**')
         st.markdown(df.shape[0])
         st.markdown('**Número de colunas:**')
@@ -37,63 +39,67 @@ def main():
         number = st.slider('Escolha o numero de colunas que deseja ver', min_value=1, max_value=20)
         st.dataframe(df.head(number))
 
-        #Passando como lista o número das colunas
+        # Passando como lista o número das colunas
         st.markdown('**Nome das colunas:**')
         st.markdown(list(df.columns))
 
-        #Cuidado ao passar os valores como índices: valores se modificam
-        #Cria-se sempre um DataDrame auxiliar (exploração) para manipulação dos dados
-        exploracao = pd.DataFrame({'nomes' : df.columns, 'tipos' : df.dtypes, 'NA #': df.isna().sum(), 'NA %' : (df.isna().sum() / df.shape[0]) * 100})
+        # Cuidado ao passar os valores como índices: valores se modificam
+        # Cria-se sempre um DataDrame auxiliar (exploração) para manipulação dos dados
+        exploracao = pd.DataFrame({'nomes': df.columns, 'tipos': df.dtypes, 'NA #': df.isna().sum(),
+                                   'NA %': (df.isna().sum() / df.shape[0]) * 100})
 
         st.markdown('**Contagem dos tipos de dados:**')
         st.write(exploracao.tipos.value_counts())
 
-        #Filtra meu dataframe onde os colunas forem tipo int64 e me retorna uma lista na tela (nomes)
+        # Filtra meu dataframe onde os colunas forem tipo int64 e me retorna uma lista na tela (nomes)
         st.markdown('**Nomes das colunas do tipo int64:**')
         st.markdown(list(exploracao[exploracao['tipos'] == 'int64']['nomes']))
 
-        #Filtrando colunas tipo float e retorno tipo nomes
+        # Filtrando colunas tipo float e retorno tipo nomes
         st.markdown('**Nomes das colunas do tipo float64:**')
         st.markdown(list(exploracao[exploracao['tipos'] == 'float64']['nomes']))
 
-        #Filtro colunas objects e retorno nomes
+        # Filtro colunas objects e retorno nomes
         st.markdown('**Nomes das colunas do tipo object:**')
         st.markdown(list(exploracao[exploracao['tipos'] == 'object']['nomes']))
 
-        #Retorna o percentual de dados faltantes
+        # Retorna o percentual de dados faltantes
         st.markdown('**Tabela com coluna e percentual de dados faltantes :**')
         st.table(exploracao[exploracao['NA #'] != 0][['tipos', 'NA %']])
 
-
         st.subheader('Inputaçao de dados númericos :')
 
-        #Como eu tiver rodando o slider todos os valores abaixo serão retornados por essa lista de nomes
-        percentual = st.slider('Escolha o limite de percentual faltante limite para as colunas vocë deseja inputar os dados', min_value=0, max_value=100)
-        lista_colunas = list(exploracao[exploracao['NA %']  < percentual]['nomes'])
+        # Como eu tiver rodando o slider todos os valores abaixo serão retornados por essa lista de nomes
+        percentual = st.slider(
+            'Escolha o limite de percentual faltante limite para as colunas vocë deseja inputar os dados', min_value=0,
+            max_value=100)
+        lista_colunas = list(exploracao[exploracao['NA %'] < percentual]['nomes'])
 
-        #Seleciona o método
+        # Seleciona o método
         select_method = st.radio('Escolha um metodo abaixo :', ('Média', 'Mediana'))
-        st.markdown('Você selecionou : ' +str(select_method))
+        st.markdown('Você selecionou : ' + str(select_method))
 
         if select_method == 'Média':
-            #Cria um novo dataframe auxiliar(df_inputado)
+            # Cria um novo dataframe auxiliar(df_inputado)
             df_inputado = df[lista_colunas].fillna(df[lista_colunas].mean())
-            exploracao_inputado = pd.DataFrame({'nomes': df_inputado.columns, 'tipos': df_inputado.dtypes, 'NA #': df_inputado.isna().sum(),
-                                       'NA %': (df_inputado.isna().sum() / df_inputado.shape[0]) * 100})
+            exploracao_inputado = pd.DataFrame(
+                {'nomes': df_inputado.columns, 'tipos': df_inputado.dtypes, 'NA #': df_inputado.isna().sum(),
+                 'NA %': (df_inputado.isna().sum() / df_inputado.shape[0]) * 100})
             st.table(exploracao_inputado[exploracao_inputado['tipos'] != 'object']['NA %'])
             st.subheader('Dados Inputados faça download abaixo : ')
-            #Download do Dataframe que foi inputado
+            # Download do Dataframe que foi inputado
             st.markdown(get_table_download_link(df_inputado), unsafe_allow_html=True)
 
-         #Input de dados com a média
+        # Input de dados com a média
         if select_method == 'Mediana':
             df_inputado = df[lista_colunas].fillna(df[lista_colunas].mean())
-            exploracao_inputado = pd.DataFrame({'nomes': df_inputado.columns, 'tipos': df_inputado.dtypes, 'NA #': df_inputado.isna().sum(),
-                                       'NA %': (df_inputado.isna().sum() / df_inputado.shape[0]) * 100})
+            exploracao_inputado = pd.DataFrame(
+                {'nomes': df_inputado.columns, 'tipos': df_inputado.dtypes, 'NA #': df_inputado.isna().sum(),
+                 'NA %': (df_inputado.isna().sum() / df_inputado.shape[0]) * 100})
             st.table(exploracao_inputado[exploracao_inputado['tipos'] != 'object']['NA %'])
             st.subheader('Dados Inputados faça download abaixo : ')
             st.markdown(get_table_download_link(df_inputado), unsafe_allow_html=True)
 
 
 if __name__ == '__main__':
-	main()
+    main()
--- Semana3/semana3.py
+++ Semana3/semana3.py
@@ -1,7 +1,11 @@
+#Imports
+#Altair Docs: https://altair-viz.github.io/
 import pandas as pd
-import streamlit as sd
+import streamlit as st
 import altair as alt
 
+#Histograma: recebe coluna e Dataframe
+#tooltip: mostra a coluna iterativa
 def criar_histograma(coluna, df):
     chart = alt.Chart(df, width=600).mark_bar().encode(
         alt.X(coluna, bin=True),
@@ -9,6 +13,7 @@ def criar_histograma(coluna, df):
     ).interactive()
     return chart
 
+#Grafico de Barras
 def criar_barras(coluna_num, coluna_cat, df):
     bars = alt.Chart(df, width=600).mark_bar().encode(
         x=alt.X(coluna_num, stack='zero'),
@@ -17,10 +22,75 @@ def criar_barras(coluna_num, coluna_cat, df):
     ).interactive()
     return bars
 
+#BoxPlot
 def criar_boxplot(coluna_num, coluna_cat, df):
-    boxplot = alt.Chart(df, width=600).mark_bar().encode(
-        x=alt.X(coluna_num, stack='zero'),
-        y=alt.Y(coluna_cat),
-        tooltip=[coluna_cat, coluna_num]
+    boxplot = alt.Chart(df, width=600).mark_boxplot().encode(
+        x=coluna_num,
+        y=coluna_cat
+    )
+    return boxplot
+
+def criar_scatterplot(x, y, color, df):
+    scatter = alt.Chart(df, width=800, height=400).mark_circle().encode(
+        alt.X(x),
+        alt.Y(y),
+        color = color,
+        tooltip = [x,y]
     ).interactive()
-    return bars)
+    return scatter
+
+def main():
+    #st.image('logo.png', width=200)
+    st.title('AcerelaDev Data Science')
+    st.subheader('Semana 3 - Análise de dados exploratória')
+    file = st.file_uploader('Escolha a base de dados que deseja analisar (.csv)', type='csv')
+    if file is not None:
+        st.subheader('Estatística descritiva univariada')
+        df = pd.read_csv(file)
+        aux = pd.DataFrame({"colunas": df.columns, 'tipos':df.dtypes})
+        colunas_numericas = list(aux[aux['tipos'] != 'object']['colunas'])
+        colunas_object = list(aux[aux['tipos'] == 'object']['colunas'])
+        colunas = list(df.columns)
+        col = st.selectbox('Selecione a coluna: ', colunas_numericas)
+        if col is not None:
+            st.markdown('Selecione o que deseja analisar: ')
+            mean = st.checkbox('Média')
+            if mean:
+                st.markdown(df[col].mean())
+            median = st.checkbox('Mediana')
+            if median:
+                st.markdown(df[col].median())
+            desvio_pad = st.checkbox('Desvio Padrão')
+            if desvio_pad:
+                st.markdown(df[col].std())
+            kurtosis = st.checkbox('Kurtosis')
+            if kurtosis:
+                st.markdown(df[col].kurtosis())
+            skewness = st.checkbox('Skewness')
+            if skewness:
+                st.markdown(df[col].skew())
+            describe = st.checkbox('Describe')
+            if describe:
+                st.table(df[colunas_numericas].describe().transpose())
+        st.subheader('Visualização dos dados')
+        st.markdown('Selecione a visualização')
+        histograma = st.checkbox('Histograma')
+        if histograma:
+            col_num = st.selectbox('Selecione a Coluna Numérica', colunas_numericas, key='unique')
+            st.markdown('Histograma da coluna: '+str(col_num))
+            st.write(criar_histograma(col_num,df))
+        barras = st.checkbox('Gráfico de Barras')
+        if barras:
+            col_num_barras = st.selectbox('Selecione a Coluna Numérica', colunas_numericas, key='unique')
+            col_cat_barras = st.selectbox('Selecione uma Coluna Categórica', colunas_object, key='unique')
+            st.markdown('Gráfico de Barras da Coluna' + str(colunas_numericas) + 'pela coluna ' +str(colunas_object))
+            st.write(criar_barras(col_num_barras, col_cat_barras, df))
+        boxplot = st.checkbox('Boxplot')
+        if boxplot:
+            col_num_barras = st.selectbox('Selecione a Coluna Numérica', colunas_numericas, key='unique')
+            col_cat_barras = st.selectbox('Selecione a uma Coluna Categórica', colunas_object, key='unique')
+            st.markdown('Gráfico de Barras da Coluna' + str(colunas_numericas) + 'pela coluna ' +str(colunas_object))
+            st.write(criar_barras(col_num_barras, col_cat_barras, df))
+
+if __name__ == '__main__':
+    main()
